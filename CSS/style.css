eb29440056d67df2e515eac13c4940fcb3a2690c
BJ Hayes <b.justinhayes@gmail.com>
2018-06-24 22:34:12 -0700

    A masterfully rendered wireframe

---
+++ CSS/style.css
@@ -0,0 +1,69 @@
+body {
+  font-family: helvetica;
+}
+
+header {
+  background: #444;
+  height: 100px;
+}
+
+.header-inner {
+  width: 960px;
+  margin: 0 auto;
+}
+
+.header-inner img {
+  float: left;
+  margin-top: 15px;
+  margin-right: 30px;
+}
+
+.header-inner li {
+  float: left;
+  color: #fff;
+  margin-right: 40px;
+  margin-top: 40px;
+}
+
+main {
+  width: 960px;
+  margin: 20px auto;
+}
+main li {
+  float: left;
+  margin-right: 30px;
+}
+
+main li:last-child {
+  margin-right: 0;
+}
+
+main section img {
+  float: left;
+  margin-right: 20px;
+}
+
+main section h2 {
+  font-size: 40px;
+  margin-bottom: 20px;
+}
+
+main section p {
+  line-height: 26px;
+}
+
+main section button {
+  border: none;
+  font-size: 24px;
+  margin-top: 20px;
+  padding: 8px;
+  border-radius: 9px;
+  background: #000;
+  color: #fff;
+}
+
+footer {
+  height: 200px;
+  background: #888;
+  clear: both;
+}
